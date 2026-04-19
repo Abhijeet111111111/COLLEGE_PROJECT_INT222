f4d377866f935b8594f04e351cc438d98b555284
Aj <yabhijeet@gmai.com>
2026-04-19 13:24:06 +0530

    fixed hotel details page

--- backend/controllers/destinationControllers.js
+++ backend/controllers/destinationControllers.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import hotelModel from './../models/hotelModel.js'
 
 export function getDestination(req, res) {
     const { id } = req.params;
@@ -12,7 +13,11 @@ export function getDestination(req, res) {
     })
 }
 
-export function renderHotelDetails(req, res) {
-    console.log('got your request')
+export async function getHotelDetails(req, res) {
+    const hotels = await hotelModel.find({ "location.city": req.query.city });
+    res.json({ hotels: JSON.stringify(hotels) })
+}
+
+export async function renderHotelDetails(req, res) {
     res.render('hotelDetails')
 }
--- backend/routes/destinationsRoutes.js
+++ backend/routes/destinationsRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getDestination,renderHotelDetails } from './../controllers/destinationControllers.js'
+import { getDestination, renderHotelDetails, getHotelDetails } from './../controllers/destinationControllers.js'
 const router = express.Router();
 
 router.get('/hotels', renderHotelDetails)
+router.get('/api/hotels', getHotelDetails)
 router.get('/:id', getDestination)
 
 
